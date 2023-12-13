451afc5db5a6359765776a5983928418117fab5e
Elant00 <132559849+Elant00@users.noreply.github.com>
2023-12-13 09:45:18 +0100

    test

---
+++ Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Grupp4CVprojekt.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public int gahbafittaFunka()
+        {
+            return 0;
+        }
+    }
+}
