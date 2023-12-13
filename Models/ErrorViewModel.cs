20b08c50df32d067639cc6f25b1e064fc8fb46de
Emil Rydberg <emilrydberg@live.se>
2023-12-13 09:35:06 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Grupp4CVprojekt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
